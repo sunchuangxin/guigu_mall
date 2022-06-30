ba061e67e18673c16853cf36caee763de7668596
sunchuangxin <2380105989@qq.com>
2022-06-30 09:53:29 +0800

    项目初始化

---
+++ gulimall-product/src/test/java/com/scx/gulimall/product/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scx.gulimall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-ware/src/test/java/com/scx/gulimall/ware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scx.gulimall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
